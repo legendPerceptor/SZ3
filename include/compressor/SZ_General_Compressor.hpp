7c4d7b4f1ed16328aaaa3087a011c172014c0b2f
Yuanjian <yuanjian@uchicago.edu>
2021-03-01 23:27:55 +0800

    BG with one more quantization bin

--- include/compressor/SZ_General_Compressor.hpp
+++ include/compressor/SZ_General_Compressor.hpp
@@ -24,13 +24,204 @@ namespace SZ {
                 fallback_predictor(LorenzoPredictor<T, N, 1>(conf.eb)),
                 predictor(predictor), quantizer(quantizer), encoder(encoder), lossless(lossless),
                 block_size(conf.block_size), stride(conf.stride),
-                global_dimensions(conf.dims), num_elements(conf.num) {
+                global_dimensions(conf.dims), num_elements(conf.num),
+                my_lorenzo(LorenzoPredictor<T, N, 1>(conf.eb)){
             static_assert(std::is_base_of_v<concepts::PredictorInterface<T, N>, Predictor>,
                           "must implement the predictor interface");
             static_assert(std::is_base_of_v<concepts::QuantizerInterface<T>, Quantizer>, "must implement the quatizer interface");
             static_assert(std::is_base_of_v<concepts::EncoderInterface<int>, Encoder>, "must implement the encoder interface");
             static_assert(std::is_base_of_v<concepts::LosslessInterface, Lossless>, "must implement the lossless interface");
         }
+
+        uchar *compress_withBG(T *data, size_t &compressed_size, T bg_data, T low_range, T high_range) {
+            auto inter_block_range = std::make_shared<SZ::multi_dimensional_range<T, N>>(data,
+                                                                                         std::begin(global_dimensions),
+                                                                                         std::end(global_dimensions),
+                                                                                         stride, 0);
+            auto intra_block_range = std::make_shared<SZ::multi_dimensional_range<T, N>>(data,
+                                                                                         std::begin(global_dimensions),
+                                                                                         std::end(global_dimensions), 1,
+                                                                                         0);
+
+            std::vector<int> quant_inds(num_elements);
+
+            T tmp;
+            for (auto element = intra_block_range->begin(); element != intra_block_range->end(); ++element) {
+                if (*element == bg_data) {
+                    quant_inds[element.get_offset()] = 2 * quantizer.get_radius() + 1;
+//                    bitmap.push_back(element.get_offset());
+                    tmp = my_lorenzo.predict(element);
+                    if(tmp >= low_range && tmp <= high_range){
+                        *element = tmp;
+                    } else {
+                        tmp = (low_range + high_range)/2;
+                        *element = tmp;
+                    }
+                }
+            }
+
+            std::array<size_t, N> intra_block_dims;
+            predictor.precompress_data(inter_block_range->begin());
+            quantizer.precompress_data();
+            size_t quant_count = 0;
+            struct timespec start, end;
+            clock_gettime(CLOCK_REALTIME, &start);
+            {
+                auto inter_begin = inter_block_range->begin();
+                auto inter_end = inter_block_range->end();
+                for (auto block = inter_begin; block != inter_end; ++block) {
+
+                    // std::cout << *block << " " << lp.predict(block) << std::endl;
+                    for (int i = 0; i < intra_block_dims.size(); i++) {
+                        size_t cur_index = block.get_local_index(i);
+                        size_t dims = inter_block_range->get_dimensions(i);
+                        intra_block_dims[i] = (cur_index == dims - 1 &&
+                                               global_dimensions[i] - cur_index * stride < block_size) ?
+                                              global_dimensions[i] - cur_index * stride : block_size;
+                    }
+
+                    intra_block_range->set_dimensions(intra_block_dims.begin(), intra_block_dims.end());
+                    intra_block_range->set_offsets(block.get_offset());
+                    intra_block_range->set_starting_position(block.get_local_index());
+                    concepts::PredictorInterface<T, N> *predictor_withfallback = &predictor;
+                    if (!predictor.precompress_block(intra_block_range)) {
+                        predictor_withfallback = &fallback_predictor;
+                    }
+                    predictor_withfallback->precompress_block_commit();
+//                    quantizer.precompress_block();
+                    auto intra_begin = intra_block_range->begin();
+                    auto intra_end = intra_block_range->end();
+                    for (auto element = intra_begin; element != intra_end; ++element) {
+                        if (quant_inds[element.get_offset()] == 2 * quantizer.get_radius() + 1) {
+                            *element = predictor_withfallback->predict(element);
+                        } else {
+                            quant_inds[element.get_offset()] = quantizer.quantize_and_overwrite(
+                                    *element, predictor_withfallback->predict(element));
+                            quant_count++;
+                        }
+                    }
+                }
+            }
+
+            clock_gettime(CLOCK_REALTIME, &end);
+            std::cout << "Predition & Quantization time = "
+                      << (double) (end.tv_sec - start.tv_sec) +
+                         (double) (end.tv_nsec - start.tv_nsec) / (double) 1000000000
+                      << "s" << std::endl;
+
+            predictor.postcompress_data(inter_block_range->begin());
+            quantizer.postcompress_data();
+
+            uchar *compressed_data = new uchar[2 * num_elements * sizeof(T)];
+            uchar *compressed_data_pos = compressed_data;
+            write(global_dimensions.data(), N, compressed_data_pos);
+            write(block_size, compressed_data_pos);
+            predictor.save(compressed_data_pos);
+            quantizer.save(compressed_data_pos);
+
+            encoder.preprocess_encode(quant_inds, 2 * quantizer.get_radius() + 2);
+            encoder.save(compressed_data_pos);
+            encoder.encode(quant_inds, compressed_data_pos);
+            encoder.postprocess_encode();
+
+            uchar *lossless_data = lossless.compress(compressed_data,
+                                                     compressed_data_pos - compressed_data,
+                                                     compressed_size);
+            lossless.postcompress_data(compressed_data);
+            return lossless_data;
+        }
+
+        T *decompress_withBG(uchar const *lossless_compressed_data, const size_t length, T bg, T low_range, T high_range) {
+            size_t remaining_length = length;
+            auto compressed_data = lossless.decompress(lossless_compressed_data, remaining_length);
+            uchar const *compressed_data_pos = compressed_data;
+            read(global_dimensions.data(), N, compressed_data_pos, remaining_length);
+            num_elements = 1;
+            for (const auto &d : global_dimensions) {
+                num_elements *= d;
+                std::cout << d << " ";
+            }
+            std::cout << std::endl;
+            read(block_size, compressed_data_pos, remaining_length);
+            stride = block_size;
+            predictor.load(compressed_data_pos, remaining_length);
+            quantizer.load(compressed_data_pos, remaining_length);
+            encoder.load(compressed_data_pos, remaining_length);
+
+            auto quant_inds = encoder.decode(compressed_data_pos, num_elements);
+            encoder.postprocess_decode();
+            lossless.postdecompress_data(compressed_data);
+
+            int const *quant_inds_pos = (int const *) quant_inds.data();
+            std::array<size_t, N> intra_block_dims;
+            auto dec_data = std::make_unique<T[]>(num_elements);
+            auto inter_block_range = std::make_shared<SZ::multi_dimensional_range<T, N>>(dec_data.get(),
+                                                                                         std::begin(global_dimensions),
+                                                                                         std::end(global_dimensions),
+                                                                                         block_size,
+                                                                                         0);
+
+            auto intra_block_range = std::make_shared<SZ::multi_dimensional_range<T, N>>(dec_data.get(),
+                                                                                         std::begin(global_dimensions),
+                                                                                         std::end(global_dimensions), 1,
+                                                                                         0);
+
+            predictor.predecompress_data(inter_block_range->begin());
+            quantizer.predecompress_data();
+            T tmp;
+            std::cout << "start decompression" << std::endl;
+            {
+                auto inter_begin = inter_block_range->begin();
+                auto inter_end = inter_block_range->end();
+                for (auto block = inter_begin; block != inter_end; block++) {
+                    for (int i = 0; i < intra_block_dims.size(); i++) {
+                        size_t cur_index = block.get_local_index(i);
+                        size_t dims = inter_block_range->get_dimensions(i);
+                        intra_block_dims[i] = (cur_index == dims - 1) ? global_dimensions[i] - cur_index * block_size
+                                                                      : block_size;
+                    }
+                    intra_block_range->set_dimensions(intra_block_dims.begin(), intra_block_dims.end());
+                    intra_block_range->set_offsets(block.get_offset());
+                    intra_block_range->set_starting_position(block.get_local_index());
+
+                    concepts::PredictorInterface<T, N> *predictor_withfallback = &predictor;
+                    if (!predictor.predecompress_block(intra_block_range)) {
+                        predictor_withfallback = &fallback_predictor;
+                    }
+                    auto intra_begin = intra_block_range->begin();
+                    auto intra_end = intra_block_range->end();
+                    for (auto element = intra_begin; element != intra_end; ++element) {
+                        if(quant_inds[element.get_offset()]==2 * quantizer.get_radius() +1){
+                            tmp = predictor_withfallback->predict(element);
+                            if(tmp>= low_range && tmp<=high_range){
+                                *element = tmp;
+                            }else{
+                                *element = (low_range+high_range)/2;
+                            }
+//                            *element = bg;
+                        }else {
+                            *element = quantizer.recover(predictor_withfallback->predict(element),
+                                                         quant_inds[element.get_offset()]);
+                        }
+                    }
+                }
+            }
+            auto intra_range = std::make_shared<SZ::multi_dimensional_range<T, N>>(dec_data.get(),
+                                                                                   std::begin(global_dimensions),
+                                                                                   std::end(global_dimensions), 1,
+                                                                                   0);
+            for(auto element = intra_range->begin(); element!=intra_range->end(); element++){
+                if(quant_inds[element.get_offset()] == 2* quantizer.get_radius()+1){
+                    *element = bg;
+                }
+            }
+
+            predictor.postdecompress_data(inter_block_range->begin());
+            quantizer.postdecompress_data();
+            return dec_data.release();
+        }
+
+
 
         uchar *compress(T *data, size_t &compressed_size) {
 
@@ -181,6 +372,7 @@ namespace SZ {
 
     private:
         Predictor predictor;
+        LorenzoPredictor<T, N, 1> my_lorenzo;
         LorenzoPredictor<T, N, 1> fallback_predictor;
         Quantizer quantizer;
         Encoder encoder;
--- test/sz_compress_test.cpp
+++ test/sz_compress_test.cpp
@@ -19,6 +19,29 @@ namespace fs = std::filesystem;
 //    auto compressed_file = SZ::readfile<unsigned char>("/Users/hython/Development/globus/output/test.dat", my_size);
 //    std::cout<<"read in size: "<<my_size<<std::endl;
 //}
+static void convert(float* data, int num) {
+    float t;
+    char *bytes;
+    for(int i=0;i<num;i++) {
+        bytes = (char*)&data[i];
+//        t = bytes[3] | (bytes[2] << 24) | (bytes[1] << 16) | (bytes[0] << 8);
+        t = bytes[3];
+        bytes[3] = bytes[0];
+        bytes[0] = t;
+        t = bytes[2];
+        bytes[2] = bytes[1];
+        bytes[1] = t;
+    }
+}
+static void printData(float* data) {
+    int z =1;
+    for (int y=0; y< 100; y++) {
+        for(int x=0;x<500;x++){
+            printf("%g  ",data[x + 500*(y+500*z)]);
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char **argv) {
 //    myfunc();
@@ -27,7 +50,12 @@ int main(int argc, char **argv) {
     auto data = SZ::readfile<float>(argv[1], num);
     std::cout << "Read " << num << " elements\n";
     std::cout << "Original Size: " << num*sizeof(float) << std::endl;
+    convert(data.get(), num);
+//    data.get()[0] = 0;
+    printData(data.get());
     float eb = 0.1;
+    float low_range=-83.00402, high_range=31.51576;
+    float bg = 1.0000000e+35;
     int count = 0;
     for(int i =0; i<num;i++) {
         if(data[i]==0){
@@ -36,20 +64,26 @@ int main(int argc, char **argv) {
     }
     std::cout<<"count: "<< count<<std::endl;
 
-    auto P_l = std::make_shared<SZ::LorenzoPredictor<float, 2, 1>>(eb);
-    auto P_reg = std::make_shared<SZ::RegressionPredictor<float, 2>>(6, 0.1 * eb);
-    std::vector<std::shared_ptr<SZ::concepts::PredictorInterface<float, 2>>> predictors_;
+    const size_t DIM = 3;
+
+    auto P_l = std::make_shared<SZ::LorenzoPredictor<float, DIM, 1>>(eb);
+//    auto P_reg = std::make_shared<SZ::RegressionPredictor<float, DIM>>(6, 0.1* eb);
+    std::vector<std::shared_ptr<SZ::concepts::PredictorInterface<float, DIM>>> predictors_;
     predictors_.push_back(P_l);
-    predictors_.push_back(P_reg);
+//    predictors_.push_back(P_reg);
 //    auto cp = std::make_shared<SZ::ComposedPredictor<float, 3>>(predictors_);
-    SZ::Config<float, 2> conf(eb, std::array<size_t, 2>{1800, 3600});
-    auto sz = SZ::SZ_General_Compressor<float, 2, SZ::ComposedPredictor<float, 2>, SZ::LinearQuantizer<float>, SZ::HuffmanEncoder<int>, SZ::Lossless_zstd>(
+    SZ::Config<float, DIM> conf(eb, std::array<size_t, DIM>{500, 500, 100});
+    auto sz = SZ::SZ_General_Compressor<float, DIM, SZ::ComposedPredictor<float, DIM>, SZ::LinearQuantizer<float>, SZ::HuffmanEncoder<int>, SZ::Lossless_zstd>(
             conf,
-            SZ::ComposedPredictor<float, 2>(predictors_),
+            SZ::ComposedPredictor<float, DIM>(predictors_),
             SZ::LinearQuantizer<float>(eb),
+//            SZ::BGDataQuantizer<float, DIM>(eb, bg,low_range, high_range),
             SZ::HuffmanEncoder<int>(),
             SZ::Lossless_zstd()
     );
+
+
+
     size_t compressed_size = 0;
     // Change to use Cpp-style timer
     // struct timespec start, end;
@@ -59,13 +93,13 @@ int main(int argc, char **argv) {
 //    err = clock_gettime(CLOCK_REALTIME, &start);
     startTime = std::chrono::system_clock::now();
     std::unique_ptr<unsigned char[]> compressed;
-    compressed.reset(sz.compress(data.get(), compressed_size));
+    compressed.reset(sz.compress_withBG(data.get(), compressed_size, bg, low_range, high_range));
 //    err = clock_gettime(CLOCK_REALTIME, &end);
     endTime = std::chrono::system_clock::now();
     std::cout << "Compression time: "
-//              << (double) (end.tv_sec - start.tv_sec) + (double) (end.tv_nsec - start.tv_nsec) / (double) 1000000000 << "s"
-                << double(std::chrono::duration_cast<std::chrono::nanoseconds>(endTime-startTime).count()) / 1000000000 << "s"
-                << std::endl;
+              //              << (double) (end.tv_sec - start.tv_sec) + (double) (end.tv_nsec - start.tv_nsec) / (double) 1000000000 << "s"
+              << double(std::chrono::duration_cast<std::chrono::nanoseconds>(endTime-startTime).count()) / 1000000000 << "s"
+              << std::endl;
     std::cout << "Compressed size = " << compressed_size << std::endl;
     std::cout << "Compression Ratio = " << num * sizeof (float)/ (float)compressed_size << std::endl;
     SZ::writefile(argv[2], compressed.get(), compressed_size);
@@ -81,16 +115,26 @@ int main(int argc, char **argv) {
     startTime = std::chrono::system_clock::now();
 //    err = clock_gettime(CLOCK_REALTIME, &start);
     std::unique_ptr<float[]> dec_data;
-    dec_data.reset(sz.decompress(compressed.get(), compressed_size));
+    dec_data.reset(sz.decompress_withBG(compressed.get(), compressed_size, bg, low_range, high_range));
 //    err = clock_gettime(CLOCK_REALTIME, &end);
     endTime = std::chrono::system_clock::now();
     std::cout << "Decompression time: "
-//              << (double) (end.tv_sec - start.tv_sec) + (double) (end.tv_nsec - start.tv_nsec) / (double) 1000000000 << "s"
-                << double(std::chrono::duration_cast<std::chrono::nanoseconds>(endTime-startTime).count()) / 1000000000 << "s"
-                << std::endl;
+              //              << (double) (end.tv_sec - start.tv_sec) + (double) (end.tv_nsec - start.tv_nsec) / (double) 1000000000 << "s"
+              << double(std::chrono::duration_cast<std::chrono::nanoseconds>(endTime-startTime).count()) / 1000000000 << "s"
+              << std::endl;
+    printData(dec_data.get());
+    auto dataV = SZ::readfile<float>(argv[1], num);
+    convert(dataV.get(), num);
     float max_err = 0;
     for (int i = 0; i < num; i++) {
-        max_err = std::max(max_err, std::abs(data[i] - dec_data[i]));
+//        max_err = std::max(max_err, std::abs(data[i] - dec_data[i]));
+        if(dataV[i] - dec_data[i] > max_err || dataV[i] - dec_data[i] < -max_err) {
+            max_err = (dataV[i] > dec_data[i]) ? dataV[i] - dec_data[i] : dec_data[i] - dataV[i];
+//            printf("NOTG data: %g, dec_data: %g, i:%d\n", dataV[i], dec_data[i], i);
+        }
+//        if(abs(dataV[i] - dec_data[i]) > 1) {
+//            printf("data: %g, dec_data: %g, i:%d\n", dataV[i], dec_data[i], i);
+//        }
     }
     std::cout << "Max error = " << max_err << std::endl;
     return 0;
